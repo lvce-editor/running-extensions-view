d19d198344a31ccb38e6eaf1630350cb60a905d5
Le Vivilet <levivilet@pm.me>
2026-07-12 19:08:43 +0200

    feature: extract running extensions css (#6)

--- packages/running-extensions-view/src/parts/RenderCss/RenderCss.ts
+++ packages/running-extensions-view/src/parts/RenderCss/RenderCss.ts
@@ -1,6 +1,8 @@
 import { ViewletCommand } from '@lvce-editor/constants'
 import type { RunningExtensionsState } from '../RunningExtensionsState/RunningExtensionsState.ts'
+import { getCss } from '../GetCss/GetCss.ts'
 
 export const renderCss = (oldState: RunningExtensionsState, newState: RunningExtensionsState): readonly any[] => {
-  return [ViewletCommand.SetCss, newState.uid, `width: ${newState.width}px; height: ${newState.height}px;`]
+  const css = getCss(newState.width, newState.height)
+  return [ViewletCommand.SetCss, newState.uid, css]
 }
