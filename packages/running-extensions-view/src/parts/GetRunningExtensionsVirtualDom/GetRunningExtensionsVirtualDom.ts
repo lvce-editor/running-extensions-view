6a5bdd96c345b2fae45db3dce19156bc8e136b90
Le Vivilet <levivilet@pm.me>
2026-07-13 17:20:59 +0200

    feature: simplify dom

--- packages/running-extensions-view/src/parts/GetRunningExtensionsVirtualDom/GetRunningExtensionsVirtualDom.ts
+++ packages/running-extensions-view/src/parts/GetRunningExtensionsVirtualDom/GetRunningExtensionsVirtualDom.ts
@@ -3,19 +3,30 @@ import type { RunningExtension } from '../RunningExtension/RunningExtension.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import { getExtensionVirtualDom } from '../GetExtensionVirtualDom/GetExtensionVirtualDom.ts'
 
-export const getRunningExtensionsVirtualDom = (extensions: readonly RunningExtension[], loaded: boolean): readonly VirtualDomNode[] => {
+const getRunningExensionsEmptyDom = (loaded: boolean) => {
   const message = loaded ? 'No running extensions' : 'Loading running extensions…'
-  const children =
-    extensions.length === 0
-      ? [
-          {
-            childCount: 1,
-            className: 'RunningExtensionsEmpty',
-            type: VirtualDomElements.Div,
-          },
-          text(message),
-        ]
-      : extensions.flatMap(getExtensionVirtualDom)
+
+  return [
+    {
+      childCount: 1,
+      className: 'RunningExtensions',
+      role: 'list',
+      type: VirtualDomElements.Div,
+    },
+    {
+      childCount: 1,
+      className: 'RunningExtensionsEmpty',
+      type: VirtualDomElements.Div,
+    },
+    text(message),
+  ]
+}
+
+export const getRunningExtensionsVirtualDom = (extensions: readonly RunningExtension[], loaded: boolean): readonly VirtualDomNode[] => {
+  if (extensions.length === 0) {
+    return getRunningExensionsEmptyDom(loaded)
+  }
+  const children = extensions.flatMap(getExtensionVirtualDom)
   return [
     {
       childCount: extensions.length === 0 ? 1 : extensions.length,
