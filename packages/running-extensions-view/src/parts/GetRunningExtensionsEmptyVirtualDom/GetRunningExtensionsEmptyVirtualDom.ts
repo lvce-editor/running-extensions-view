5d19f8e76f792014ebbb70dc001ed88e92e1548f
Le Vivilet <levivilet@pm.me>
2026-07-19 17:35:10 +0200

    feature: move empty dom into separate file (#28)

---
+++ packages/running-extensions-view/src/parts/GetRunningExtensionsEmptyVirtualDom/GetRunningExtensionsEmptyVirtualDom.ts
@@ -0,0 +1,23 @@
+import { type VirtualDomNode, text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
+
+export const getRunningExtensionsEmptyDom = (loaded: boolean): readonly VirtualDomNode[] => {
+  const message = loaded ? 'No running extensions' : 'Loading running extensions…'
+
+  return [
+    {
+      childCount: 1,
+      className: 'RunningExtensions',
+      onContextMenu: DomEventListenerFunctions.HandleContextMenu,
+      role: AriaRoles.List,
+      type: VirtualDomElements.Div,
+    },
+    {
+      childCount: 1,
+      className: 'RunningExtensionsEmpty',
+      type: VirtualDomElements.Div,
+    },
+    text(message),
+  ]
+}
