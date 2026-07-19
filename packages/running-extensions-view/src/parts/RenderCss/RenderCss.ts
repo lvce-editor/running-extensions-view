15beefbd93bd96e6faa1abf5d25590770cf5ed1d
Le Vivilet <levivilet@pm.me>
2026-07-19 15:45:45 +0000

    fix: sort destructured state fields

--- packages/running-extensions-view/src/parts/RenderCss/RenderCss.ts
+++ packages/running-extensions-view/src/parts/RenderCss/RenderCss.ts
@@ -3,7 +3,7 @@ import type { RunningExtensionsState } from '../RunningExtensionsState/RunningEx
 import { getCss } from '../GetCss/GetCss.ts'
 
 export const renderCss = (oldState: RunningExtensionsState, newState: RunningExtensionsState): readonly any[] => {
-  const { width, height, uid } = newState
+  const { height, uid, width } = newState
   const css = getCss(width, height)
   return [ViewletCommand.SetCss, uid, css]
 }
