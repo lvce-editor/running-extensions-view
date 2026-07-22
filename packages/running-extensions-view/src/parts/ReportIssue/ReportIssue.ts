2ef63ab1efb9586d13acf702d105864a866b4e99
Le Vivilet <levivilet@pm.me>
2026-07-22 22:51:14 +0200

    feature: simplify issue report

--- packages/running-extensions-view/src/parts/ReportIssue/ReportIssue.ts
+++ packages/running-extensions-view/src/parts/ReportIssue/ReportIssue.ts
@@ -4,6 +4,27 @@ import type { RunningExtensionsState } from '../RunningExtensionsState/RunningEx
 import { getIssuesUrl } from '../GetIssuesUrl/GetIssuesUrl.ts'
 import * as RunningExtensionsStrings from '../RunningExtensionsStrings/RunningExtensionsStrings.ts'
 
+const OpenCommand = {
+  External: 1,
+  Url: 2,
+}
+
+const getOpenCommand = (platform: number): number => {
+  if (platform === PlatformType.Electron) {
+    return OpenCommand.External
+  } else {
+    return OpenCommand.Url
+  }
+}
+
+const applyOpenCommand = async (cmd: number, issuesUrl: string): Promise<void> => {
+  if (cmd === OpenCommand.External) {
+    await RendererWorker.openExternal(issuesUrl)
+  } else {
+    await RendererWorker.openUrl(issuesUrl)
+  }
+}
+
 export const reportIssue = async (state: RunningExtensionsState, index: number): Promise<RunningExtensionsState> => {
   const { extensions, platform } = state
   const issuesUrl = getIssuesUrl(extensions[index]?.repository)
@@ -11,10 +32,7 @@ export const reportIssue = async (state: RunningExtensionsState, index: number):
     await RendererWorker.confirm(RunningExtensionsStrings.reportingIssuesNotSupported())
     return state
   }
-  if (platform === PlatformType.Electron) {
-    await RendererWorker.openExternal(issuesUrl)
-  } else {
-    await RendererWorker.openUrl(issuesUrl)
-  }
+  const cmd = getOpenCommand(platform)
+  await applyOpenCommand(cmd, issuesUrl)
   return state
 }
