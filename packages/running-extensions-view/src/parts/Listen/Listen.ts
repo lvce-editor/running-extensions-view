23332c80904b0768be884f7b1c57bbe48a0f686b
Le Vivilet <levivilet@pm.me>
2026-07-22 22:45:41 +0200

    feature: simplify rpc init (#66)

--- packages/running-extensions-view/src/parts/Listen/Listen.ts
+++ packages/running-extensions-view/src/parts/Listen/Listen.ts
@@ -1,17 +1,18 @@
-import { WebWorkerRpcClient } from '@lvce-editor/rpc'
-import { RendererWorker } from '@lvce-editor/rpc-registry'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
 import { initializeClipBoardWorker } from '../InitializeClipBoardWorker/InitializeClipBoardWorker.ts'
 import { initializeExtensionManagementWorker } from '../InitializeExtensionManagementWorker/InitializeExtensionManagementWorker.ts'
 import { initializeMainProcess } from '../InitializeMainProcess/InitializeMainProcess.ts'
+import { initializeRendererWorker } from '../InitializeRendererWorker/InitializeRendererWorker.ts'
 import { initializeSharedProcess } from '../InitializeSharedProcess/InitializeSharedProcess.ts'
 import { registerCommands } from '../RunningExtensionsStates/RunningExtensionsStates.ts'
 
 export const listen = async (): Promise<void> => {
   registerCommands(CommandMap.commandMap)
-  const rpc = await WebWorkerRpcClient.create({
-    commandMap: CommandMap.commandMap,
-  })
-  RendererWorker.set(rpc)
-  await Promise.all([initializeClipBoardWorker(), initializeExtensionManagementWorker(), initializeMainProcess(), initializeSharedProcess()])
+  await Promise.all([
+    initializeRendererWorker(),
+    initializeClipBoardWorker(),
+    initializeExtensionManagementWorker(),
+    initializeMainProcess(),
+    initializeSharedProcess(),
+  ])
 }
